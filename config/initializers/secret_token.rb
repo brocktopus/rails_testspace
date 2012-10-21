a36b92ddf26a393579630153a7d2d7ff62fea663
Kevin Brock <ikatowi@gmail.com>
2012-10-20 20:42:32 -0400

    starting over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTestspace::Application.config.secret_token = 'b46b0c25664c73cf383277be50a7a23a49724e7a64b1723573b65ed2f4bc75d68eb3ed8c4ca8f3ab5a59f57d49bf37a9e74d16dc44be182105480153f6581d11'
